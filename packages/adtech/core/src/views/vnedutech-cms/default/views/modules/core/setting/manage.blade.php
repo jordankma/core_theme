39da7b9da9a65acfbb7115b667540e95267b428d
Điện PHP <diennh@vnedutech.vn>
2018-09-07 11:03:00 +0700

    test

--- packages/adtech/core/src/views/vnedutech-cms/default/views/modules/core/setting/manage.blade.php
+++ packages/adtech/core/src/views/vnedutech-cms/default/views/modules/core/setting/manage.blade.php
@@ -163,10 +163,25 @@
     <script src="{{ config('site.url_static') . ('/vendor/laravel-filemanager/js/lfm.js') }}" ></script>
     <script>
         $(function () {
-            var domain = "http://dhcd.vnedutech.vn/admin/laravel-filemanager/";
+            var domain = "http://dhcd-release.vnedutech.vn/administrator/laravel-filemanager";
             $('#lfm').filemanager('image', {prefix: domain});
             $('#lfm1').filemanager('image', {prefix: domain});
             $('#lfm2').filemanager('image', {prefix: domain});
+
+            window.addEventListener('message', function(event) {
+
+                // IMPORTANT: Check the origin of the data!
+                if (~event.origin.indexOf('http://dhcd.vnedutech.vn')) {
+                    // The data has been sent from your site
+
+                    // The data sent with postMessage is stored in event.data
+                    console.log(event.data);
+                } else {
+                    // The data hasn't been sent from your site!
+                    // Be careful! Do not use it.
+                    return;
+                }
+            });
         })
     </script>
 @stop
