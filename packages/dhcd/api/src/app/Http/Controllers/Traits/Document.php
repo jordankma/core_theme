367fe732ef092024b84936b12f09723f51610110
Điện PHP <diennh@vnedutech.vn>
2018-09-09 12:53:40 +0700

    update api

--- packages/dhcd/api/src/app/Http/Controllers/Traits/Document.php
+++ packages/dhcd/api/src/app/Http/Controllers/Traits/Document.php
@@ -708,4 +708,13 @@ trait Document
         $data = str_replace('null', '""', $data);
         return $data;
     }
+
+    function is_url($uri){
+        if(preg_match( '/^(http|https):\\/\\/[a-z0-9_]+([\\-\\.]{1}[a-z_0-9]+)*\\.[_a-z]{2,5}'.'((:[0-9]{1,5})?\\/.*)?$/i' ,$uri)){
+            return $uri;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- packages/dhcd/api/src/app/Http/Controllers/Traits/Setting.php
+++ packages/dhcd/api/src/app/Http/Controllers/Traits/Setting.php
@@ -121,4 +121,13 @@ trait Setting
                 }';
         return response($data)->setStatusCode(200)->header('Content-Type', 'application/json; charset=utf-8');
     }
+
+    function is_url($uri){
+        if(preg_match( '/^(http|https):\\/\\/[a-z0-9_]+([\\-\\.]{1}[a-z_0-9]+)*\\.[_a-z]{2,5}'.'((:[0-9]{1,5})?\\/.*)?$/i' ,$uri)){
+            return $uri;
+        }
+        else{
+            return false;
+        }
+    }
 }
