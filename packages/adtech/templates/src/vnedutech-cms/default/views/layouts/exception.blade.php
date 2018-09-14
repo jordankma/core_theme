24fcb91662f47c66a79695a5acc685fc56914ca9
Điện PHP <diennh@vnedutech.vn>
2018-09-14 15:38:33 +0700

    update blade

--- packages/adtech/templates/src/vnedutech-cms/default/views/layouts/exception.blade.php
+++ packages/adtech/templates/src/vnedutech-cms/default/views/layouts/exception.blade.php
@@ -6,10 +6,10 @@
     <title>Error page | Josh Admin Template</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- global level js-->
-    <link href="{{ asset('/vendor/vnedutech-cms/default/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- end of globallevel js-->
     <!-- page level styles-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('/vendor/vnedutech-cms/default/css/pages/404.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/css/pages/404.css') }}"/>
     <!-- end of page level styles-->
 </head>
 
@@ -25,16 +25,16 @@
 @yield('content')
 
 <!-- global js -->
-<script src="{{ asset('/vendor/vnedutech-cms/default/js/jquery-1.11.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/js/jquery-1.11.1.min.js') }}" type="text/javascript"></script>
 <!-- Bootstrap -->
-<script src="{{ asset('/vendor/vnedutech-cms/default/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <!--livicons-->
-<script src="{{ asset('/vendor/vnedutech-cms/default/js/raphael-min.js') }}"></script>
-<script src="{{ asset('/vendor/vnedutech-cms/default/js/livicons-1.4.min.js') }}"></script>
-<script src="{{ asset('/vendor/vnedutech-cms/default/js/holder.js') }}"></script>
+<script src="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/js/raphael-min.js') }}"></script>
+<script src="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/js/livicons-1.4.min.js') }}"></script>
+<script src="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/js/holder.js') }}"></script>
 <!-- end of global js -->
 <!-- begining of page level js-->
-<script src="{{ asset('/vendor/vnedutech-cms/default/js/404.js') }}"></script>
+<script src="{{ config('site.url_static') . ('/vendor/vnedutech-cms/default/js/404.js') }}"></script>
 <!-- end of page level js-->
 </body>
 </html>
