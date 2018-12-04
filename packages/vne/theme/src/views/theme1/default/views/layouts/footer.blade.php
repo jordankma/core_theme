c94f5b6d57ec4f7d091fcf2733183b5ddccf434c
tuan <tuanlv@vnedutech.vn>
2018-12-04 14:26:36 +0700

    no message

--- packages/vne/theme/src/views/theme1/default/views/layouts/footer.blade.php
+++ packages/vne/theme/src/views/theme1/default/views/layouts/footer.blade.php
@@ -1,21 +1,14 @@
 <footer class="footer">
 	<div class="footer-top">
 		<div class="container">
-			<div class="row">
-				{{-- <div class="col-md-4">
-					{!! isset($SETTING['info_footer_4']) ? $SETTING['info_footer_4'] : '' !!}	
+			<div class="carousel js-carousel-03">
+				@if(!empty($list_don_vi_tai_tro))
+				@foreach($list_don_vi_tai_tro as $element)
+				<div class="carousel-item">
+					<a href="{{ $element->link }}"><img src="{{ $element->img }}" alt=""></a>
 				</div>
-				<div class="col-md-8"> --}}
-					<div class="carousel js-carousel-03">
-						@if(!empty($list_don_vi_tai_tro))
-						@foreach($list_don_vi_tai_tro as $element)
-						<div class="carousel-item">
-							<a href="{{ $element->link }}"><img src="{{ $element->img }}" alt=""></a>
-						</div>
-						@endforeach
-						@endif
-					</div>
-				{{-- </div> --}}
+				@endforeach
+				@endif
 			</div>
 		</div>
 	</div>
--- packages/vne/theme/src/views/theme1/default/views/layouts/index/_logo_group.blade.php
+++ packages/vne/theme/src/views/theme1/default/views/layouts/index/_logo_group.blade.php
@@ -1,21 +1,21 @@
 <div class="col-12">
-				<!-- logo list -->
-				<section class="section logo-group">
-					<div class="logo-list">
-						<h2 class="title">Ban tổ chức cuộc thi</h2>
-						<div class="carousel js-carousel-01">
-							@if(!empty($list_don_vi_dong_hanh))
-							@foreach($list_don_vi_dong_hanh as $element)
-							<a class="carousel-item" href="{{ $element->comlink }}">
-								<div class="logo">
-									<img src="{{ $element->img }}" alt="">
-								</div>
-								<h3 class="name">{{ $element->comname }}</h3>
-							</a>
-							@endforeach
-							@endif
-						</div>
+	<!-- logo list -->
+	<section class="section logo-group">
+		<div class="logo-list">
+			<h2 class="title">Ban tổ chức cuộc thi</h2>
+			<div class="carousel js-carousel-01">
+				@if(!empty($list_don_vi_dong_hanh))
+				@foreach($list_don_vi_dong_hanh as $element)
+				<a class="carousel-item" href="{{ $element->comlink }}">
+					<div class="logo">
+						<img src="{{ $element->img }}" alt="">
 					</div>
-				</section>
-				<!-- logo list end -->
+					<h3 class="name">{{ $element->comname }}</h3>
+				</a>
+				@endforeach
+				@endif
 			</div>
+		</div>
+	</section>
+	<!-- logo list end -->
+</div>
